3c8d30ba823a60c7b463dc9fdc59714dddeec7c9
Noah Feasey Perkins <noah.feasey-perkins@thornewidgery.co.uk>
2024-12-20 12:26:33 +0000

    Finalised day 4 pt. 1

--- 4/part1.py
+++ 4/part1.py
@@ -1,22 +1,26 @@
 import re
 
-def boundary(y,x):
-    #row and colummn indexes cannot exceed 9 or be less than 0
-    limit = True
-    if y > 9 or y < 0 or x > 9 or x < 0:
-        limit = False
-    return limit
-
-data = open('4/ex input.txt','r')
-directions = [(-1,-1),(0,-1),(1,-1),(-1,0),(1,0),(1,-1),(1,0),(1,1)]
+data = open('4/input.txt','r')
+grid = data.read().split()
+directions = [(-1,-1),(0,-1),(1,-1),(-1,0),(1,0),(-1,1),(0,1),(1,1)]
 next_letter = ["M","A","S"]
+count = 0
 
-for y_index,row in enumerate(data):
+def boundary(y,x):
+    # row and colummn indexes cannot exceed 9 or be less than 0
+    return 0 <= y < len(grid) and 0 <= x < len(grid[0])
+
+for y_index,row in enumerate(grid):
     for x_index,character in enumerate(row):
         if character == "X":
-            for y_var,x_var in enumerate(directions):
-                course = (y_index + y_var,x_index + x_var)
-                if boundary(course[0],course[1]):
-                    next_chara = 
-
-print(course," - ",limit)
+            for y_var,x_var in directions:
+                course = (y_index,x_index)
+                for letter in next_letter:
+                    course = (course[0] + y_var,course[1] + x_var)
+                    if not boundary(course[0],course[1]):
+                        break
+                    if not grid[course[0]][course[1]] == letter:
+                        break
+                    if letter == "S":
+                        count += 1
+print(count)
